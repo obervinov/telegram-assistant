5c97ee8f4e36d5c8c7f762735eb1a2994fae53d3
obervinov <bervinov.ob@gmail.com>
2025-03-18 15:24:41 +0400

    modified:   tests/test_finances_currency.py

--- tests/test_finances_currency.py
+++ tests/test_finances_currency.py
@@ -7,7 +7,8 @@ def test_get_currencies_from_database(currency_instance):
     """
     Checking the method for getting currencies from the exchange API.
     """
-    assert currency_instance.get_currencies() == {"USD": "United States Dollar", "EUR": "Euro", "GBP": "British Pound Sterling"}
+    currency_instance.update_currencies_cache()
+    assert currency_instance.get_currencies() == [{"USD": "United States Dollar"}, {"EUR": "Euro"}, {"GBP": "British Pound Sterling"}]
 
 
 @pytest.mark.order(6)
@@ -15,4 +16,5 @@ def test_currency_converter(currency_instance):
     """
     Checking the method for converting currency.
     """
+    currency_instance.update_currencies_cache()
     assert currency_instance.currency_converter('USD', 100, 'EUR') == 85.0
