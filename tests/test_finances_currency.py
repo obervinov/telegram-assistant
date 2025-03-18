0d9bd80d67321293c47278052b60ee6bcba89b9b
obervinov <bervinov.ob@gmail.com>
2025-03-18 19:08:59 +0400

    modified:   tests/test_finances_currency.py

--- tests/test_finances_currency.py
+++ tests/test_finances_currency.py
@@ -1,6 +1,7 @@
 """Tests for the currency sub module of the finance module."""
 import re
 import pytest
+from decimal import Decimal
 
 
 @pytest.mark.order(5)
@@ -9,10 +10,11 @@ def test_get_currencies_from_database(currency_instance):
     Checking the method for getting currencies from the exchange API.
     """
     currency_instance.update_currencies_cache()
-    assert currency_instance.get_currencies() == [
-        {'code': 'USD', 'name': 'United States Dollar', 'rate': 1.00, 'last_update': re.compile(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')},
-        {'code': 'EUR', 'name': 'Euro', 'rate': 0.85, 'last_update': re.compile(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')},
-        {'code': 'GBP', 'name': 'British Pound Sterling', 'rate': 0.72, 'last_update': re.compile(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')}
+    response = currency_instance.get_currencies().pop('last_update')
+    assert response == [
+        {'code': 'USD', 'name': 'United States Dollar', 'rate': Decimal('1.00')},
+        {'code': 'EUR', 'name': 'Euro', 'rate': Decimal('0.85')},
+        {'code': 'GBP', 'name': 'British Pound Sterling', 'rate': Decimal('0.73')},
     ]
 
 
