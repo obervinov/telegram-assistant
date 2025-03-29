1cec37c8b83ec0745eb819a8c1ae0c40cfa8360a
obervinov <bervinov.ob@gmail.com>
2025-03-29 17:29:02 +0400

    modified:   CHANGELOG.md modified:   tests/test_finances_income.py

--- tests/test_finances_income.py
+++ tests/test_finances_income.py
@@ -13,12 +13,12 @@ def test_add_income_to_database(finance_income_instance, database_class):
         'name': 'Deposit', 'description': 'Passive income', 'category': 'Deposit', 'currency': 'USD', 'amount': 1000,
         'start_date': '2021-01-01', 'expiration_date': '2022-01-01', 'interest_rate': 10, 'tax': 5
     }
-    finance_income_instance.income(**salary_data)
-    finance_income_instance.income(**coinbox_data)
-    finance_income_instance.income(**deposit_data)
+    finance_income_instance.income(user_id='111', **salary_data)
+    finance_income_instance.income(user_id='111', **coinbox_data)
+    finance_income_instance.income(user_id='111', **deposit_data)
 
-    db_salary_data = database_class.get_finance_income(income_name='Salary')
-    db_coinbox_data = database_class.get_finance_income(income_name='CoinBox')
+    db_salary_data = database_class.get_finance_income(user_id='111', income_name='Salary')
+    db_coinbox_data = database_class.get_finance_income(user_id='111', income_name='CoinBox')
     # db_deposit_data = database_class.get_finance_income(income_name='Deposit')
 
     assert db_salary_data is not None
