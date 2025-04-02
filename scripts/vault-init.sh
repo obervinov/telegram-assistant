9697a42c620028cf17e169f1069e3f0998695dac
obervinov <bervinov.ob@gmail.com>
2025-04-02 15:58:34 +0400

    fixed mismatch arguments in messages.json

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -38,10 +38,4 @@ vault write telegram-assistant-database/roles/telegram-assistant-users \
     revocation_statements="REVOKE ALL PRIVILEGES ON SCHEMA public FROM \"{{name}}\"; REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM \"{{name}}\"; REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM \"{{name}}\"; DROP ROLE \"{{name}}\";" \
     default_ttl="24h" \
     max_ttl="72h"
-vault write telegram-assistant-database/roles/telegram-assistant-users-rl \
-    db_name=postgresql \
-    creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT ALL PRIVILEGES ON SCHEMA public TO \"{{name}}\"; GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO \"{{name}}\"; GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO \"{{name}}\";" \
-    revocation_statements="REVOKE ALL PRIVILEGES ON SCHEMA public FROM \"{{name}}\"; REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM \"{{name}}\"; REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM \"{{name}}\"; DROP ROLE \"{{name}}\";" \
-    default_ttl="24h" \
-    max_ttl="72h"
 # End of snippet
